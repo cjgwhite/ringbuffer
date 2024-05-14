d8532666b4d4f457082be72191e55ef5f31d1776
christopher.white <christopher.white@engineering.digital.dwp.gov.uk>
2024-05-14 08:44:06 +0100

    add valid construct test

--- src/test/java/com/phoundation/ring/RingBufferTest.java
+++ src/test/java/com/phoundation/ring/RingBufferTest.java
@@ -1,8 +1,10 @@
 package com.phoundation.ring;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -27,6 +29,25 @@ public class RingBufferTest {
   void noNegativeCapacity(int capacity, boolean blocking) {
     assertThrows(
         IllegalArgumentException.class,
+        () -> new RingBuffer<String>(capacity, blocking)
+    );
+  }
+
+  @ParameterizedTest
+  @CsvSource(
+      delimiterString = "|",
+      useHeadersInDisplayName = true,
+      textBlock = """
+          capacity    | blocking
+          1           | true
+          2           | true
+          1           | false
+          2           | false
+          """
+  )
+  @DisplayName("Builds instance with valid values")
+  void validConstruct(int capacity, boolean blocking) {
+    assertDoesNotThrow(
         () -> new RingBuffer<String>(capacity, blocking)
     );
   }
